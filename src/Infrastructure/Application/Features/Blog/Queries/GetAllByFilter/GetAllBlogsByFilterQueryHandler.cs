24d108f455b352b1980d2d2e88c0a0bae181b022
navidkhanjari <developer1.fika@gmail.com>
2025-05-28 14:34:25 +0330

    fix some tests

--- src/Infrastructure/Application/Features/Blog/Queries/GetAllByFilter/GetAllBlogsByFilterQueryHandler.cs
+++ src/Infrastructure/Application/Features/Blog/Queries/GetAllByFilter/GetAllBlogsByFilterQueryHandler.cs
@@ -1,15 +1,13 @@
-﻿using Microsoft.Extensions.Caching.Distributed;
-
+﻿
 namespace Application.Features.Blog.Queries.GetAllByFilter;
 
-public class GetAllBlogsByFilterQueryHandler(IDistributedCache distributedCache, IUnitOfWork unitOfWork, IMapper mapper)
+public class GetAllBlogsByFilterQueryHandler(IUnitOfWork unitOfWork, IMapper mapper)
     : IRequestHandler<GetAllBlogsByFilterQuery, BlogFilterViewModel>
 {
     #region ( Dependencies )
 
     protected BlogFilterViewModel BlogFilterViewModel { get; set; }
 
-    protected IDistributedCache DistributedCache { get; } = distributedCache;
     protected IUnitOfWork UnitOfWork { get; } = unitOfWork;
     protected IMapper Mapper { get; } = mapper;
 
